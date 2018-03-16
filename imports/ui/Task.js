6b80377d9390736b3f664c31379a503393dfa8fe
Emanoel <emanoelromano@gmail.com>
2018-03-16 03:54:03 -0300

    corrida

---
+++ imports/ui/Task.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Tasks } from "../api/tasks";
+
+// Task component represents a single todo item
+export default class Task extends Component {
+    toggleChecked() {
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
+    deleteThisTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
+    render() {
+        // Give tasks a diferent className when they are checked off,
+        // so that we can style them nicely in CSS
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+        return (
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+                    &times;
+                </button>
+
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className="text">
+                    <strong>{this.props.task.username}</strong>: {this.props.task.text}
+                </span>
+            </li>
+        );
+    }
+}
